5483f4397d33323e950e7fe435693cbf67e7d7f5
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-09-20 21:08:29 +1000

    Add main.js (with object plans) to index header

---
+++ main.js
@@ -0,0 +1,14 @@
+// need the object model
+
+// Nodes HAVE:
+//  exits
+//  rules (for direct access e.g. warp songs, loading game, current location)
+//  solutions for reaching them
+// Exits HAVE:
+//  rules
+//  a node (probably)
+// Rules HAVE:
+//  A set function that resolves down to a true/false
+//  A list of things that that function relies on
+// solutions HAVE....
+//  I dunno yet
