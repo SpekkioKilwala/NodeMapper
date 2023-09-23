a976c29f71d1b36ded9ebe2a187192ba168b4bc7
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-09-23 23:56:27 +1000

    Make nodes be direct members of nodeContainer

--- main.js
+++ main.js
@@ -21,12 +21,17 @@ class Node {
     }
 }
 
-const nodeMap = {
-    nodes: [],
+const nodeContainer = {
+    // I don't know what this thing actually does except be a container for all the nodes.
+    // The nodes are all just members of this, because that means that you can get a direct
+    // reference to any node if you just know the name, which is important.
+    // It CAN be responsible for creating the links and setting the rules,
+    // but I'm pretty sure that makes it the "god-object" and that's to be avoided.
 }
 
-// add "home", "field", and "castle"
-
-nodeMap.nodes.push(new Node("home"));
-nodeMap.nodes.push(new Node("field"));
-nodeMap.nodes.push(new Node("castle"));
+// This could all be hardcoded inside the nodeContainer when I create it,
+// but I assume that I want to be reading all this from a JSON file later
+// or something.
+nodeContainer["home"] = new Node("home");
+nodeContainer["field"] = new Node("field");
+nodeContainer["castle"] = new Node("castle");
